e7c350b81e392425e709e1388a94ef156adb808c
Adam Farley <adfarley@redhat.com>
2021-01-29 09:51:43 +0000

    debug 3

--- get.sh
+++ get.sh
@@ -72,6 +72,7 @@ parseCommandLineArgs()
     eval `rolelist`;
     eval `rolelist -e`;
     echo "aixtools debug finished";
+    exit(99);
 	while [[ $# -gt 0 ]] && [[ ."$1" = .-* ]] ; do
 		opt="$1";
 		shift;
